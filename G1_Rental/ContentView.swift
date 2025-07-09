8860ea126256ddc6723f76bdb99b3e9fcb1a402e
Darshhhhh <contact.shahdarsh@gmail.com>
2025-07-08 22:32:13 -0400

    Initial Commit

---
+++ G1_Rental/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  G1_Rental
+//
+//  Created by Darsh on 2025-07-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ G1_Rental/G1_RentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  G1_RentalApp.swift
+//  G1_Rental
+//
+//  Created by Darsh on 2025-07-08.
+//
+
+import SwiftUI
+
+@main
+struct G1_RentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
